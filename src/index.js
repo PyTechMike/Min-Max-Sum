af6f7e3e9380cf0bb08b97029e618c786d734e65
PyTechMike <technomike04@gmail.com>
2017-12-12 15:46:27 +0200

    Homework

---
+++ src/index.js
@@ -0,0 +1,27 @@
+﻿function isNumber(value) {
+	return (typeof value === 'number'); 
+}
+
+function min (numbers) {
+	return Math.min.apply(Math, numbers.filter(isNumber));
+}
+
+function max (numbers) {
+	return Math.max.apply(Math, numbers.filter(isNumber));
+}
+
+function sum() {
+	var args = []
+				.slice
+				.call(arguments)
+				.filter(isNumber);	
+	var sum = args.reduce(function(previousValue, currentValue) {
+		return currentValue + previousValue;
+	})
+	return sum;
+}
+
+console.log(sum(1, 99, 'flood'));
+console.log(max(['teeext', 43, 99]));
+console.log(min(['feef', 43, 99]));
+
